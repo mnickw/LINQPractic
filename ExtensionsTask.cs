35771dfab15f12882b4b81c295a555677f47dcd1
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 22:33:11 +0300

    Initial project

---
+++ ExtensionsTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_slideviews
+{
+	public static class ExtensionsTask
+	{
+		/// <summary>
+		/// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
+		/// Медиана списка из четного количества элементов — это среднее арифметическое 
+        /// двух серединных элементов списка после сортировки.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
+		public static double Median(this IEnumerable<double> items)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <returns>
+		/// Возвращает последовательность, состоящую из пар соседних элементов.
+		/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
+		/// </returns>
+		public static IEnumerable<Tuple<T, T>> Bigrams<T>(this IEnumerable<T> items)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
